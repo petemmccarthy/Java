f66ebd3737ea05cc865468be302866d8ba19ba5c
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-08 18:42:10 +0000

    first code written

--- SineWave.java
+++ SineWave.java
@@ -1,1 +1,13 @@
 import java.awt.*;
+import java.awt.geom.*;
+import javax.swing.*;
+
+public class SineWave
+{
+    public static void main ( String[] args )
+    {
+      SineFrame frame = new SineFrame();
+      frame.setDefaultCloseOperatioin(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
+    }
+}
