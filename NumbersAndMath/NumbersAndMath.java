b7f5b4897e543cd1215b3975f46dd3c0fd51f8ae
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-09 16:35:15 +0000

    layout tweaks

--- NumbersAndMath/NumbersAndMath.java
+++ NumbersAndMath/NumbersAndMath.java
@@ -2,7 +2,7 @@ public class NumbersAndMath
 {
   public static void main ( String[] args )
   {
-    System.out.println( " I will now count my chickens:");
+    System.out.println( "I will now count my chickens:");
 
     System.out.println( "Hens " + ( 25 + 30 / 6 ) );
     System.out.println( "Roosters " + ( 100 - 25 * 3 % 4 ) );
