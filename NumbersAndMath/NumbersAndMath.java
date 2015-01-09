d8d7862e811711ef9869c3c6271b0a51c51a8952
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-09 16:16:04 +0000

    chickens counted

--- NumbersAndMath/NumbersAndMath.java
+++ NumbersAndMath/NumbersAndMath.java
@@ -2,6 +2,10 @@ public class NumbersAndMath
 {
   public static void main ( String[] args )
   {
+    System.out.println( " I will now count my chickens:");
+
+    System.out.println( "Hens " + ( 25 + 30 / 6 ) );
+    System.out.println( "Roosters " + ( 100 - 25 * 3 % 4 ) );
     
   }
 }
