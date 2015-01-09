0770424a4e268c5f53e811d06d5271be93bffdda
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-09 16:24:53 +0000

    eggs counted

--- NumbersAndMath/NumbersAndMath.java
+++ NumbersAndMath/NumbersAndMath.java
@@ -6,6 +6,14 @@ public class NumbersAndMath
 
     System.out.println( "Hens " + ( 25 + 30 / 6 ) );
     System.out.println( "Roosters " + ( 100 - 25 * 3 % 4 ) );
+
+    System.out.println( "Now I will count the eggs:");
+
+    System.out.println( 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 );
+
+    System.out.println( "Is it true that 3 + 2 < 5 - 7?" );
+
+    System.out.println( 3 + 2 < 5 - 7 );
     
   }
 }
