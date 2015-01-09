2431c4299e9e88c64b8956ffe4a9764277f9baeb
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-09 16:30:59 +0000

    more code added

--- NumbersAndMath/NumbersAndMath.java
+++ NumbersAndMath/NumbersAndMath.java
@@ -14,6 +14,17 @@ public class NumbersAndMath
     System.out.println( "Is it true that 3 + 2 < 5 - 7?" );
 
     System.out.println( 3 + 2 < 5 - 7 );
-    
+
+    System.out.println( "What is 3 + 2? " + ( 3 + 2 ) );
+    System.out.println( "What is 5 - 7? " + ( 5 - 7 ) );
+
+    System.out.println( "Oh, that's why it's false." );
+
+    System.out.println( "How about some more." );
+
+    System.out.println( "Is it greater? " + ( 5 > -2 ) );
+    System.out.println( "Is it greater or equal? " + ( 5 >= -2 ) );
+    System.out.println( "Is it less or equal? " + ( 5 <= -2 ) );
+  
   }
 }
