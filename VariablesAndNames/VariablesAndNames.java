d2814aabbe91274bfa8a0c738e4ad1bffb632101
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-09 17:37:32 +0000

    exercise finished

--- VariablesAndNames/VariablesAndNames.java
+++ VariablesAndNames/VariablesAndNames.java
@@ -13,5 +13,12 @@ public class VariablesAndNames
     cars_driven = drivers;
     carpool_capacity = cars_driven * space_in_a_car;
     average_passengers_per_car = passengers / cars_driven;
+
+    System.out.println( "There are " + cars + " cars available." );
+    System.out.println( "There are obly " + drivers + " drivers available." );
+    System.out.println( "There will be " + cars_not_driven + " empty cars today." );
+    System.out.println( "We can transport " + carpool_capacity + " people today." );
+    System,out.println( "We have " + passengers + " to carpool today." );
+    System.out.println( "We need to put about " + average_passengers_per_car + " in each car." );
   }
 }
