68b6c5a9f5009f158f09465267b86fc9df4391cb
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-10 17:52:27 +0000

    variables added

--- AskingQuestions/AskingQuestions.java
+++ AskingQuestions/AskingQuestions.java
@@ -5,6 +5,20 @@ public class AskingQuestions
   public static void main( String[] args )
   {
     Scanner keyboard = new Scanner(System.in);
+
+    int age;
+    String height;
+    double weight;
+
+    System.out.print( "How old are you? " );
+    age = keyboard.nextInt();
+
+    System.out.print( "How tall are you? " );
+    height = keyboard.next();
+
+    System.out.print( "How much do you weigh? " );
+    weight = keyboard.nextDouble();
+
     
   }
 }
