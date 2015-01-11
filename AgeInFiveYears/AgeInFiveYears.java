19fbf985b4d3ddb5efe69d805ebcc234c82fcda2
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-11 09:34:17 +0000

    data input coded

--- AgeInFiveYears/AgeInFiveYears.java
+++ AgeInFiveYears/AgeInFiveYears.java
@@ -4,6 +4,16 @@ public class AgeInFiveYears
 {
   public static void main ( String[] args )
   {
+    Scanner keyboard = new Scanner(System.in);
+
+    String name;
+    int age;
+    
+    System.out.print( "Hello. What is your name? ");
+    name = keyboard.next();
+
+    System.out.print( "Hi " + name + " How old are you? " );
+    age = keyboard.nextInt();
 
   }
 }
