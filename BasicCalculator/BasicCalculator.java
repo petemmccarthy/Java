a506e555983bf05b08c31d981534071799965062
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-11 09:49:50 +0000

    calculator inpt variables declared

--- BasicCalculator/BasicCalculator.java
+++ BasicCalculator/BasicCalculator.java
@@ -4,6 +4,12 @@ public class BasicCalculator
 {
   public static void main ( String[] args )
   {
-    
+
+    Scanner keyboard = new Scanner(System.in);
+
+    int num1;
+    int num2;
+    int num3;
+
   }
 }
