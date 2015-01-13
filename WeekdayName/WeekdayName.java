e7629271e02721a297e074cad2d3ab16fbd90eab
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-13 21:19:34 +0000

    output coded

--- WeekdayName/WeekdayName.java
+++ WeekdayName/WeekdayName.java
@@ -4,6 +4,7 @@ public class WeekdayName
 {
   public static String weekday_name( int weekday )
   {
+
     String result = "";
 
     if ( weekday == 1 )
@@ -14,37 +15,30 @@ public class WeekdayName
     {
       result = "Monday";
     }
-
     else if ( weekday == 3 )
     {
       result = "Tuesday";
     }
-
     else if ( weekday == 4 )
     {
       result = "Wednesday";
     }
-
     else if ( weekday == 5 )
     {
       result = "Thursday";
     }
-
     else if ( weekday == 6 )
     {
       result = "Friday";
     }
-
     else if ( weekday == 7 )
     {
       result = "Saturday";
     }
-
     else if ( weekday == 0 )
     {
       result = "Saturday";
     }
-
     else
     {
       result = "error"
@@ -53,4 +47,27 @@ public class WeekdayName
     return result;
 
   }
+
+  public static void main( String[] args )
+  {
+    System.out.println( "weekday 1: " + weekday_name(1) );
+    System.out.println( "weekday 2: " + weekday_name(2) );
+    System.out.println( "weekday 3: " + weekday_name(3) );
+    System.out.println( "weekday 4: " + weekday_name(4) );
+    System.out.println( "weekday 5: " + weekday_name(5) );
+    System.out.println( "weekday 6: " + weekday_name(6) );
+    System.out.println( "weekday 7: " + weekday_name(7) );
+    System.out.println( "weekday 0: " + weekday_name(8) );
+    System.out.println();
+    System.out.println( "weekday 43: " + weekday_name(43) );
+    System.out.println( "weekday 17: " + weekday_name(17) );
+    System.out.println( "weekday -1: " + weekday_name(-1) );
+
+    
+  }
+
 }
+
+
+
+
