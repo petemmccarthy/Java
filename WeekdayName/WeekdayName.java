e01b7521ca846d426e36997081ca2cb3d5e8a7e0
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-13 21:14:16 +0000

    string to int matching

--- WeekdayName/WeekdayName.java
+++ WeekdayName/WeekdayName.java
@@ -4,6 +4,53 @@ public class WeekdayName
 {
   public static String weekday_name( int weekday )
   {
-    
+    String result = "";
+
+    if ( weekday == 1 )
+    {
+      result = "Sunday";
+    }
+    else if ( weekday == 2 )
+    {
+      result = "Monday";
+    }
+
+    else if ( weekday == 3 )
+    {
+      result = "Tuesday";
+    }
+
+    else if ( weekday == 4 )
+    {
+      result = "Wednesday";
+    }
+
+    else if ( weekday == 5 )
+    {
+      result = "Thursday";
+    }
+
+    else if ( weekday == 6 )
+    {
+      result = "Friday";
+    }
+
+    else if ( weekday == 7 )
+    {
+      result = "Saturday";
+    }
+
+    else if ( weekday == 0 )
+    {
+      result = "Saturday";
+    }
+
+    else
+    {
+      result = "error"
+    }
+
+    return result;
+
   }
 }
