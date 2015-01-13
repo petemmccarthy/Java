2f1a19ee1f9741c3dd4c34ec716a94689c193c1a
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-13 18:50:32 +0000

    ppl vs cars elseif

--- ElseAndIf/ElseAndIf.java
+++ ElseAndIf/ElseAndIf.java
@@ -6,5 +6,20 @@ public class ElseAndIf
     int cars = 40
     int buses = 15;
 
+    if ( cars > people )
+    {
+      System.out.println( "We should take the cars." );
+    }
+
+    else if ( cars < people )
+    {
+      System.out.println( "We should not take the cars." ); 
+    }
+
+    else
+    {
+      System.out.println( "We can't decide." );
+    }
+    
   }
 }
