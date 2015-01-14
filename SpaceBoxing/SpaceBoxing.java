ee6bcb69aaa1f120d6b1324220d7396b75b77837
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-14 16:10:44 +0000

    planets coded

--- SpaceBoxing/SpaceBoxing.java
+++ SpaceBoxing/SpaceBoxing.java
@@ -13,5 +13,30 @@ public class SpaceBoxing
       result = 0.78;
     }
 
+    if ( planet == 2 )
+    {
+      result = 0.39;
+    }
+
+    if ( planet == 3 )
+    {
+      result = 2.65;
+    }
+
+    if ( planet == 4 )
+    {
+      result = 1.17;
+    }
+
+    if ( planet == 5 )
+    {
+      result = 1.05;
+    }
+
+    if ( planet == 6 )
+    {
+      result = 1.23;
+    }
+
   }
 }
