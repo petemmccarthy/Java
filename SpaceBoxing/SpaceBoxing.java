c48cefa7cc246891c4b3a968cbc2302f425de33d
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-14 16:14:26 +0000

    minor tweaks

--- SpaceBoxing/SpaceBoxing.java
+++ SpaceBoxing/SpaceBoxing.java
@@ -13,30 +13,32 @@ public class SpaceBoxing
       result = 0.78;
     }
 
-    if ( planet == 2 )
+    else if ( planet == 2 )
     {
       result = 0.39;
     }
 
-    if ( planet == 3 )
+    else if ( planet == 3 )
     {
       result = 2.65;
     }
 
-    if ( planet == 4 )
+    else if ( planet == 4 )
     {
       result = 1.17;
     }
 
-    if ( planet == 5 )
+    else if ( planet == 5 )
     {
       result = 1.05;
     }
 
-    if ( planet == 6 )
+    else ( planet == 6 )
     {
       result = 1.23;
     }
+
+    return result;
 
   }
 }
