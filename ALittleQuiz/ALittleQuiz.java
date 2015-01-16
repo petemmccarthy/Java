63c3c2bca4835b4e365d1cc75b7ef339aaefe6c3
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-16 19:03:17 +0000

    ans1 logic coded

--- ALittleQuiz/ALittleQuiz.java
+++ ALittleQuiz/ALittleQuiz.java
@@ -26,14 +26,19 @@ public class ALittleQuiz
       System.out.println( "That's right!" );
     }
 
+    else
+    {
+      System.out.println( "That's incorrect!")
+    }
 
-    System.out.println( "Q1) Can you store the value \"cat\" in a variable of type int?");
+
+    System.out.println( "\nQ1) Can you store the value \"cat\" in a variable of type int?");
     System.out.println( "1) yes");
     System.out.println( "2) no");
     System.out.println( "\n> ");
     answer2 = keyboard.nextInt();
 
-    System.out.println( "Q1) What is the result of 9+6/3?");
+    System.out.println( "\nQ1) What is the result of 9+6/3?");
     System.out.println( "1) 5");
     System.out.println( "2) 11");
     System.out.println( "3) 15/3");
@@ -43,3 +48,5 @@ public class ALittleQuiz
   }
   
 }
+
+
