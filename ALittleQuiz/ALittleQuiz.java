249c9bdfb9cff0b47f039364c87cefe0ff722ea1
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-16 16:16:37 +0000

    intro

--- ALittleQuiz/ALittleQuiz.java
+++ ALittleQuiz/ALittleQuiz.java
@@ -6,6 +6,11 @@ public class ALittleQuiz
   public static void main( String[] args )
   {
 
+    Scanner keyboard = new SCanner(System.in);
+
+    System.out.println( " Are you ready for a quiz? " );
+    System.out.println( "Okay, here it comes!" );
+
   }
   
 }
