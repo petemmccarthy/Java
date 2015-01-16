19a24d09f0c499d27cdfeef2de4c8a2086cb407e
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-16 18:02:34 +0000

    answers input coded

--- ALittleQuiz/ALittleQuiz.java
+++ ALittleQuiz/ALittleQuiz.java
@@ -19,20 +19,22 @@ public class ALittleQuiz
     System.out.println( "2) Anchorage");
     System.out.println( "3) Juneau");
     System.out.println( "\n> ");
+    answer1 = keyboard.nextInt();
 
 
     System.out.println( "Q1) Can you store the value \"cat\" in a variable of type int?");
     System.out.println( "1) yes");
     System.out.println( "2) no");
     System.out.println( "\n> ");
-
+    answer2 = keyboard.nextInt();
 
     System.out.println( "Q1) What is the result of 9+6/3?");
     System.out.println( "1) 5");
     System.out.println( "2) 11");
     System.out.println( "3) 15/3");
     System.out.println( "\n> ");
-
+    answer3 = keyboard.nextInt();
+    
   }
   
 }
