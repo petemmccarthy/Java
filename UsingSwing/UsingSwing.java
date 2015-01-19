3e9928c89a70cb084b8c6ecb649c74596bf588ce
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-19 10:48:43 +0000

    output and var declared

--- UsingSwing/UsingSwing.java
+++ UsingSwing/UsingSwing.java
@@ -6,6 +6,11 @@ public class Prog611
   public static void main ( String[] args )
   {
 
+    String name = JOptionPane.showInputDialog( "What is your name?" );
 
+    String name = JOptionPane.showInputDialog( "How old are you?" );
+    int age = Integer.parseInt( input );
+
+    
   }
 }
