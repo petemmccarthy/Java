f1d4cd047ccf8d1f51c5b48a3a0b627ac3534017
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-19 20:15:30 +0000

    first question

--- TwentyQuestions/TwentyQuestions.java
+++ TwentyQuestions/TwentyQuestions.java
@@ -8,7 +8,14 @@ public class TwentyQuestions
 
     Scanner keyboard = new Scanner(System.in);
 
-    int ans1, ans2;
+    String ans1, ans2;
+
+    System.out.print( "TWO QUESTIONS!" );
+    System.out.print( "Think of an object, and I'll try to guess it." );
+
+    System.out.print( "Question 1) Is it animal, vegetable or mineral?" );
+    System.out.print( ">" );
+    int ans1 = keyboard.next();
     
   }
 }
