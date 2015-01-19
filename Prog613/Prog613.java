557503d1fa3c70594c6b0db39106e130521faea4
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-19 16:32:16 +0000

    prog613 class complete

--- Prog613/Prog613.java
+++ Prog613/Prog613.java
@@ -4,7 +4,13 @@ import java.awt.*;
 
 public class Prog613
 {
+  public static void main( String[] args )
+  {
 
+    Frame613 f = new Frame613();
+    f.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+    f.setVisible( true );
+  }
 }
 
 class Frame613 extends JFrame
