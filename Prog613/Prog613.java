a066f3fe6b904b6eea48b3108c785f404275ea8a
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-19 16:35:53 +0000

    frame complete

--- Prog613/Prog613.java
+++ Prog613/Prog613.java
@@ -6,7 +6,6 @@ public class Prog613
 {
   public static void main( String[] args )
   {
-
     Frame613 f = new Frame613();
     f.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
     f.setVisible( true );
@@ -16,6 +15,16 @@ public class Prog613
 class Frame613 extends JFrame
 {
 
+  public Frame613()
+  {
+    setTitle( "613 rocks!" );
+    setSize( 300, 200);
+    setLocation( 100, 200 );
+
+    Panel613 panel = new Panel613();
+    Container cp = getContentPane();
+    cp.add( panel );
+  }
 }
 
 class Panel613 extends JPanel
