f80868202c14f75b070bc00c78293910b289bf1d
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-19 10:51:53 +0000

    var name changed

--- UsingSwing/UsingSwing.java
+++ UsingSwing/UsingSwing.java
@@ -8,13 +8,13 @@ public class Prog611
 
     String name = JOptionPane.showInputDialog( "What is your name?" );
 
-    String name = JOptionPane.showInputDialog( "How old are you?" );
+    String input = JOptionPane.showInputDialog( "How old are you?" );
     int age = Integer.parseInt( input );
 
     System.out.print( "Hello, " + name + "." );
     System.out.print( "Next year, you'll be " + ( age + 1) )
 
     System.exit(0);
-    
+
   }
 }
