95e7b5948d366e1469750dc24dc250d99947ab07
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-19 16:38:33 +0000

    panel complete

--- Prog613/Prog613.java
+++ Prog613/Prog613.java
@@ -30,4 +30,10 @@ class Frame613 extends JFrame
 class Panel613 extends JPanel
 {
 
+  public void paintComponent( Graphics g )
+  {
+
+    super.paintComponent( g );
+    g.drawString( "Hi", 75, 100 );
+  }
 }
