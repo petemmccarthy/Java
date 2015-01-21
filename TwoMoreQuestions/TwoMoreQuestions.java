bd30e9f391afbc8283be7c5c19e788b82a16380e
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-21 17:20:58 +0000

    first option complete

--- TwoMoreQuestions/TwoMoreQuestions.java
+++ TwoMoreQuestions/TwoMoreQuestions.java
@@ -11,6 +11,12 @@ public class TwoMoreQuestions
 
     String ans1, ans2;
 
-    
+    System.out.println( "TWO MORE QUESTIONS, BABY" );
+
+    System.out.println( "Think of something and I'll try to guess it!" );
+
+    System.out.print( "Question 1) Does it belong inside, outside or both? " );
+    ans1 = keyboard.next();
+
   }
 }
