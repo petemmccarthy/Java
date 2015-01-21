3e56b3b185198e4e70669c4e5e962f7ab265fce6
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-21 17:16:21 +0000

    class struc, keyboard input and vars declared

--- TwoMoreQuestions/TwoMoreQuestions.java
+++ TwoMoreQuestions/TwoMoreQuestions.java
@@ -2,5 +2,15 @@ import java.util.Scanner;
 
 public class TwoMoreQuestions
 {
-  
+
+  public static void main( String[] args )
+
+  {
+
+    Scanner keyboard = new Scanner(System.in);
+
+    String ans1, ans2;
+
+    
+  }
 }
