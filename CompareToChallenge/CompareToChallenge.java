6aa8773b93af2590e46291023cf9dfcc247d4337
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-22 10:50:41 +0000

    first compareTo added

--- CompareToChallenge/CompareToChallenge.java
+++ CompareToChallenge/CompareToChallenge.java
@@ -3,4 +3,13 @@ import java.util.GregorianCalendar;
 public class CompareToChallenge
 {
   
+  public static void main( String[] args )
+
+  {
+
+    System.out.print( "Comparing \"axe\" with \"dog\" produces " );
+    int i = "axe".compareTo("dog");
+    System.out.println(i);
+
+  }
 }
