f4dee1cb4ac6db1ea3be113391a073c4af10a13e
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-23 11:53:01 +0000

    user input type and vars declared

--- NumberGuess/NumberGuess.java
+++ NumberGuess/NumberGuess.java
@@ -1,11 +1,16 @@
 import java.util.Random;
 
+import java.util.Scanner;
+
 public class NumberGuess
 {
 
   public static void main ( String[] args )
   {
 
+    Scanner keyboard = new Scanner(System.in);
+
+    int secret_number = 4;
     
   }
 }
