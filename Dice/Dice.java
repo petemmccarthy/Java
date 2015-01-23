616129910b69b6a584909ee2ab507474888e03b7
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-23 17:56:22 +0000

    class, vars and random object created

--- Dice/Dice.java
+++ Dice/Dice.java
@@ -7,6 +7,16 @@ public class Dice
 
   public static void main ( String[] args )
   {
+
+    Scanner keyboard = new Scanner(System.in);
+
+    Random r = new Random();
+
+    int roll1 = 1 + r.nextInt(6);
+
+    int roll1 = 1 + r.nextInt(6);
+
     
+
   }
 }
