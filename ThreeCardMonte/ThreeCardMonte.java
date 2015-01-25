b5876b381f067e7b81bc65ea692077ac269b92f7
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-25 18:49:13 +0000

    user story coded

--- ThreeCardMonte/ThreeCardMonte.java
+++ ThreeCardMonte/ThreeCardMonte.java
@@ -8,6 +8,10 @@ public class ThreeCardMonte
   public static void mian ( String[] args )
   {
 
-    
+    System.out.println( "You slide up to Fast Eddie's card table and plop down your cash." );
+    System.out.println( "He glances at you out of the corner of his eye and starts shuffling." );
+    System.out.println( "He lays down three cards." );
+    System.out.println( "\nWhich one is the ace?" );
+
   }
 }
