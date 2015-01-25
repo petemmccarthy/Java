67fc56e0d4548dd961359c78100922e844e8b055
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-25 18:27:29 +0000

    scanner object and var declared

--- OneShotHiLo/OneShotHiLo.java
+++ OneShotHiLo/OneShotHiLo.java
@@ -8,6 +8,12 @@ public class OneShotHiLo
   public static void main ( String[] args )
   {
 
+    Scanner keyboard = new Scanner(System.in);
+
+    Random r = new Random();
+
+    int cookie = 1 + r.nextInt(100);
+
     
   }
 }
