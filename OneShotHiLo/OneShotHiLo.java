d6674268313fbd0d805290281895f781f99e7ab9
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-25 18:39:05 +0000

    typo

--- OneShotHiLo/OneShotHiLo.java
+++ OneShotHiLo/OneShotHiLo.java
@@ -15,24 +15,23 @@ public class OneShotHiLo
     int randy_num = 1 + r.nextInt(100);
     int guess;
 
-    Sysytem.out.print( "I'm thinking of a numnber between 1-100. Try and guess it: ");
+    System.out.print( "I'm thinking of a numnber between 1-100. Try and guess it: ");
     guess= keyboard.nextInt();
 
     if ( guess == randy_num )
     {
-      Sysytem.out.print( "Dead right! It was " + randy_num + "!");  
+      System.out.println( "Dead right! It was " + randy_num + "!");  
     }
 
     else if ( guess > randy_num )
     {
-      Sysytem.out.print( "Sorry you were too high, it was " + randy_num );  
+      System.out.println( "Sorry you were too high, it was " + randy_num );  
     }
 
     else
     {
-      Sysytem.out.print( "Sorry you were too low, it was " + randy_num );  
+      System.out.println( "Sorry you were too low, it was " + randy_num );  
     }
-    
 
   }
 }
