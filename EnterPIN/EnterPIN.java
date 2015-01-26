61627ac1982af475100e5f6fa4bd4f4af7fc92ab
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-26 18:54:48 +0000

    welcome msg coded

--- EnterPIN/EnterPIN.java
+++ EnterPIN/EnterPIN.java
@@ -6,6 +6,13 @@ public class EnterPIN
   public static void main ( String[] args )
   {
 
-    
+    Scanner keyboard = new Scanner(System.in);
+    int pin = 12345;
+
+    System.out.println("WELCOME TO THE BANK.");
+    System.out.print("PLEASE ENTER YOUR PIN: ");
+    int entry = keyboard.nextInt();
+
+
   }
 }
