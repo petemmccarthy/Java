513e05859695c52c83a5ec67b129ab370c7c4a40
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-01-26 18:07:04 +0000

    user input and random object created

--- ThreeCardMonte/ThreeCardMonte.java
+++ ThreeCardMonte/ThreeCardMonte.java
@@ -8,10 +8,18 @@ public class ThreeCardMonte
   public static void mian ( String[] args )
   {
 
+    Scanner keyboard = new Scanner(System.in);
+
+    Random r = new Random();
+
+    int ace = 1 + r.nextInt(3);
+    int guess;
+
     System.out.println( "You slide up to Fast Eddie's card table and plop down your cash." );
     System.out.println( "He glances at you out of the corner of his eye and starts shuffling." );
     System.out.println( "He lays down three cards." );
-    System.out.println( "\nWhich one is the ace?" );
+    System.out.println( "\nWhich card is the ace, 1, 2 or 3?" );
+    guess = keyboard.nextInt();
 
     System.out.println( "Ha! Fast Eddie wins again! The ace was card number 3." );
     System.out.println( "You nailed it! Fast Eddie reluctantly hands over your winnings, scowling." );
